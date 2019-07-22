7f08caa8cb438881cda387d223288032e9989d84
pragmatic-memory-1672 <hp.mars.le@gmail.com>
2019-07-22 22:03:17 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,40 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  #array.index(value_to_find)
+  
+  counter = 0 
+  while counter < array.length do
+    if array[counter] == value_to_find 
+      return counter
+    end
+    counter += 1
+  end 
 end
 
 def find_max_value(array)
-  # Add your solution here
+  #array.max
+  
+  max_num = 0
+  index = 0 
+  while index < array.length do
+    if array[index] > max_num
+      max_num = array[index]
+    end   
+    index += 1 
+  end
+  return max_num
 end
 
 def find_min_value(array)
-  # Add your solution here
+  #array.min
+  
+  min_value = array[0]
+  index = 0 
+  
+  while index < array.length do
+    if array[index] < min_value
+      min_value = array[index]
+    end 
+    index += 1 
+  end 
+  return min_value
 end
