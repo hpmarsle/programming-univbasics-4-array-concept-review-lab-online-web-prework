971762faefe4afb00eed7a7e86f91fcad3b5d6e3
pragmatic-memory-1672 <hp.mars.le@gmail.com>
2019-07-22 21:56:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,11 +9,15 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  #initialize max var 
-  max_num
-  #need to compare if this number in the array is greater than the last number
-  #go to next index 
-  #array.max
+  max_num = 0
+  index = 0 
+  while index < array.length do
+    if array[index] > max_num
+      max_num = array[index]
+    end   
+    index += 1 
+  end   
+    #array.max
 end
 
 def find_min_value(array)
